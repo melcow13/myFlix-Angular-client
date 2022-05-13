350771e4d4c7e14dbd176fdb8cb3ef29ceb7d9ce
Melody Kao <melody.ch.kao@gmail.com>
2022-05-13 18:20:24 +0200

    correct login api

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -29,8 +29,8 @@ ngOnInit(): void {
 }
 
 // This is the function responsible for sending the form inputs to the backend
-registerUser(): void {
-    this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
   // Logic for a successful user registration goes here! (To be implemented)
      this.dialogRef.close(); // This will close the modal on success!
      this.snackBar.open(result, 'OK', {
